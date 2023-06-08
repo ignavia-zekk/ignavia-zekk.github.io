f556ef51c55de0a0b578da6633462168e6212ab1
ignavia-zekk <75147773+ignavia-zekk@users.noreply.github.com>
2023-06-08 11:28:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,136 @@
+<html>
+<head>
+  <title>WEB-BASED BOOK SEARCH, CIRCULATION AND RESERVATION</title>
+</head>
+<style media="screen">
+body
+{
+background-image: url('wpics/logologo.jpg');
+background-repeat: no-repeat;
+background-attachment: fixed;
+background-size: 100% 100%;
+margin-top: 190px;
+}
+a
+{
+ position: relative;
+  display: inline-block;
+
+  color: #33C4FF;
+  background-image: url("wpics/background2.jpg");
+  text-transform: uppercase;
+  letter-spacing:  4px;
+  text-decoration: none;
+  font-size: 45px;
+}
+a:hover
+{
+  color: #255784;
+  background: #B2FF33;
+  box-shadow: 0 0 90px #B2FF33, 0 0 90px #B2FF33, 0 0 90px #B2FF33;
+}
+b
+{
+  color: #33C4FF;
+  background-image: url("wpics/background2.jpg");
+  box-shadow: 0 0 0px #93FF33, 0 0 00px #93FF33, 0 0 00px #93FF33;
+  text-decoration: none;
+  font-size: 20px;
+}
+c
+{
+  color: #255784;
+  background: #93FF33;
+  box-shadow: 0 0 0px #93FF33, 0 0 0px #93FF33, 0 0 0px #93FF33;
+  text-decoration: none;
+  font-size: 20px;
+}
+d
+{
+  color: #255784;
+  background: #33FFBE;
+  box-shadow: 0 0 0px #33FFBE, 0 0 0px #33FFBE, 0 0 0px #33FFBE;
+  text-decoration: none;
+  font-size: 20px;
+}
+e
+{
+  color: #255784;
+  background: #33DDFF;
+  box-shadow: 0 0 0px #33DDFF, 0 0 0px #33DDFF, 0 0 0px #33DDFF;
+  text-decoration: none;
+  font-size: 20px;
+}
+
+
+</style>
+<body>
+<section>
+  <form>
+    <b>
+      LIBRARY HOURS
+    </b>
+    <br>
+   <c>
+    Regular School Term
+    <br>
+    Monday to Friday: 7:30am-7:00pm
+    <br>
+    Saturday:8:00am-12:00pm
+  </c>
+  <br>
+  <d>
+  Semestral Break
+  <br>
+  Monday to Friday: 8:00am-12:00noon
+  <br>
+  1:00pm-5:00pm
+</d>
+    <br>
+    <e>
+    Summer Term
+      <br>
+      Monday to Saturday: 7:30am-5:00pm
+      <e/>
+    <br>
+    <br>
+    <br>
+    <br>
+<div class="">
+  <a href="librarian/login.php">
+  LIBRARIAN
+  </a>
+</div>
+
+<div class="">
+  <a href="admin/login.php">
+  ADMIN
+  </a>
+</div>
+
+  <div class="">
+    <a href="user/login.php">
+      USER
+    </a>
+  </div>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <div class="">
+    <a href="https://plt-borderlessschool.com/">
+    Borderless Website
+  </a>
+  </div>
+</form>
+</section>
+</body>
+</html>
